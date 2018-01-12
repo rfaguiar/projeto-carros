11e2dcbd8c97043de8662e4f7fc38e6687c12337
Rogerio Aguiar <rfaguiar1@gmail.com>
2018-01-12 19:26:12 -0200

    implementado deleção de recurso veiculo

--- carros-api/src/main/java/br/com/carros/carrosapi/resource/VeiculoResource.java
+++ carros-api/src/main/java/br/com/carros/carrosapi/resource/VeiculoResource.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -50,6 +51,12 @@ public class VeiculoResource {
 		Veiculo veiculoSalvo = this.veiculoRepo.save(veiculo);
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, veiculoSalvo.getId()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(veiculoSalvo);
+	}
+	
+	@DeleteMapping(path="/{codigo}")
+	public ResponseEntity<Void> delete (@PathVariable Long codigo) {
+		this.veiculoRepo.delete(codigo);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
